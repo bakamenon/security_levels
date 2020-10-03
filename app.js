7ad2b27521baa4b15be4e517630ca9c833bc17c6
Vijay Menon <themenon@Vijays-MacBook-Air.local>
2020-10-03 15:47:34 +0900

    Add Google OAUTH2, try Facebook OAUTH2

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const { trim, result } = require("lodash");
+const findOrCreate = require("mongoose-findorcreate");
+
 // Cookies and Sessions
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+
+//Google OAUTH2
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
@@ -44,10 +49,13 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String,
+  facebookId: String,
 });
 
 // To use sessions - 3 Attach the plugin  passport-local-mongoose before the model is created
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
@@ -55,11 +63,43 @@ const User = mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 
 //TODO: Explore the below options
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+//Google OAUTH2 comes *AFTER* the Passport create startegy, sereialize and deserialse is done
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
 
 app.get("/", function (req, res) {
   res.render("home");
+});
+
+//Auth with google OAUTH2
+app.get("/auth/google", passport.authenticate("google", { scope: ["profile"] }));
+
+//... And once the auth is successful route it to required page or if failed, go back to login page
+app.get("/auth/google/secrets", passport.authenticate("google", { failureRedirect: "/login" }), function (req, res) {
+  res.redirect("/secrets");
 });
 
 app.get("/login", function (req, res) {
@@ -102,6 +142,7 @@ app.post("/register", function (req, res) {
   });
 });
 
+//Auth with local auth (Traditional email / password)
 app.post("/login", function (req, res) {
   const user = new User({
     username: req.body.username,
